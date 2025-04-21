45931b2713d8d40a252f7cec938649aa323ff895
Valival-05 <familleabadiebompard@gmail.com>
2025-04-21 19:10:22 +0200

    Update Bot Discord.py

--- Bot Discord.py
+++ Bot Discord.py
@@ -14,22 +14,7 @@ bot = commands.Bot(command_prefix='/', intents=discord.Intents.default())
 
 @bot.event
 async def on_ready():
-    print(f"[DEBUG] Bot connecté en tant que {bot.user.name} ({bot.user.id})")
-
-    try:
-        channel = await bot.fetch_channel(1353147720864501764)
-        print(f"[DEBUG] Canal trouvé : {channel.name}")
-
-        await channel.send("🎉 Je suis maintenant en ligne et prêt à vous aider ! 🎉")
-        await asyncio.sleep(1)
-
-        await channel.send("Je suis en train de démarrer ...")
-        await asyncio.sleep(1)
-
-        await channel.send("Les LOG ne sont pas encore disponibles.")
-    
-    except Exception as e:
-        print(f"[ERREUR] Impossible d'envoyer le message : {e}")
+    print("[DEBUG] Je suis dans on_ready()")
 
 @bot.command(help="Affiche un mot nommé Pong !")
 async def Ping(ctx):
